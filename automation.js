fdbe2902c097caee33a0a654de3cbd30f03e5fb5
niva10 <nivlim18@gmail.com>
2024-11-11 23:03:05 +0200

    Update automation.js

--- automation.js
+++ automation.js
@@ -3,16 +3,16 @@ const puppeteer = require('puppeteer');
 
 async function runAutomation() {
 
-  const browser = await puppeteer.launch({ headless: false });      // "Headless: false" to see the browser
-  const page = await browser.newPage();                            // Opens a new page
-  await page.goto('https://testsite.getjones.com/ExampleForm/');  // Navigates to test url
+  const browser = await puppeteer.launch({ headless: false }); 
+  const page = await browser.newPage();                          
+  await page.goto('https://testsite.getjones.com/ExampleForm/');  
 
-  await page.type('input[name="name"]', 'Niv');
-  await page.type('input[name="email"]', 'niv123@Stam.com');
-  await page.type('input[name="phone"]', '0521234567');
-  await page.type('input[name="company"]', 'ABC company');
+  await page.type('#name', 'Niv');
+  await page.type('#email', 'niv123@stam.com');
+  await page.type('#phone', '0521234567');
+  await page.type('#company', 'ABC company');
 
-  await page.select('select[name="number_of_employees"]', '51-500');  // Bonus
+  await page.select('#employees', '51-500'); 
 
   await page.screenshot({ path: 'before_click.png' });
   
@@ -20,10 +20,11 @@ async function runAutomation() {
 
   await page.waitForSelector('.copyright')? console.log('Reached the thank you page'): '';
 
-  // await browser.close();  
 }
 
 runAutomation();
+
+
 
 
 
